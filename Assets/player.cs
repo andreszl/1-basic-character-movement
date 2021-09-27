14ef375ebc2bec2bd8e932359dd46c0b62e1e319
andreszl <andreszl.devcode@gmail.com>
2021-09-27 06:36:35 -0500

    initial commit

---
+++ Assets/player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("I'm a cube");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0f, Input.GetAxis("Vertical") * Time.deltaTime);   
+    }
+}
